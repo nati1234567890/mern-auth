d10f97afde8058d6391215f422449960709ad804
natnael <natiizelalem@gmail.com>
2024-01-25 08:15:53 -0800

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("listen on port 3000");
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("connected to db listen on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("error connecteing to db");
+  });
